7fca7b9aad5d0007e57e6a45374c131a040e6429
ravi <ravinderpalsinghkhokhar@gmail.com>
2023-12-25 22:39:27 +0530

    added wordle path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,24 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import HomeLayout from "../layouts/home";
 import Games from "../views/home";
 import TicTacToe from "../views/tic-tac-toe";
+import Wordle from "../views/wordle";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
-    children : [
-      {index: true, element: <Games/>},
+    children: [
+      { index: true, element: <Games /> },
       {
-        path: 'tic-tac-toe',
-        element: <TicTacToe/>
-      }
-    ]
+        path: "tic-tac-toe",
+        element: <TicTacToe />,
+      },
+      {
+        path: "wordle",
+        element: <Wordle />,
+      },
+    ],
   },
 ]);
 
