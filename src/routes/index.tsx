5ed17e7d453fa2a75c0c575c1c9bdc26245fede1
ravi <ravinderpalsinghkhokhar@gmail.com>
2024-01-09 22:44:13 +0530

    added tetris game

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import Games from "../views/home";
 import TicTacToe from "../views/tic-tac-toe";
 import Wordle from "../views/wordle";
 import Snake from "../views/snake";
+import Tetris from "../views/tetris";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
       {
         path: "snake",
         element: <Snake />,
+      },
+      {
+        path: "tetris",
+        element: <Tetris />,
       },
     ],
   },
